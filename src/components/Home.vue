10389e3cc5fb1de802ecd77aef02f48503d0d412
Munitor10 <rafamartinsmb10@gmail.com>
2025-03-11 21:22:14 -0300

    bem vindo name

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,35 @@
-<template>
- <div>Home
+<template>  
+    <div>
+    <div class="container mt-4 text-center">
+    <h2>Bem vindo, {{ nomeUsuarioLogado }}</h2>
+    </div>
+
     <br>
-    <button @cl>Sair</button> 
- </div>
+        <button @click="deslogar">Sair</button>
+    </div>
 </template>
 
-<script>
+<script>   
 export default {
-  methods:
+    data(){
+        return{
+            nomeUsuarioLogado:''
+        };
+    },
+    mounted(){
+        const usuarioLogado = localStorage.getItem('dados-usuario-logado');
+        if (usuarioLogado){
+            this.nomeUsuarioLogado = JSON.parse(usuarioLogado).nome;
+        }
+    },
+    methods: {
+        deslogar()
+        {
+            localStorage.removeItem('dados-usuario-logado');
+            this.$router.push('/login');
+        }
+    }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
